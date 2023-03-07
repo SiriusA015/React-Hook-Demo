37149833d47527f9f7038fa51c2ea39855f5d8f8
Bzzard <sirius.a015@gmail.com>
2023-03-07 19:43:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-20 h-20"></div>
+      <div className="w-20 h-20"></div>
+      <div className="w-20 h-20"></div>
+      <div className="flex">
+        <div>first</div>
+        <div>second</div>
+      </div>
+      <div className="w-20 h-20"></div>
+      <div className="w-20 h-20"></div>
+    </div>
+  );
+}
+
+export default App;
